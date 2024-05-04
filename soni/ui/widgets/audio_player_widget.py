642fd9b8aa8a03668e1716062f8e100dbfc2df6e
unknown <frogibogidogi@gmail.com>
2024-05-04 10:37:37 +1000

    Player play

--- soni/ui/widgets/audio_player_widget.py
+++ soni/ui/widgets/audio_player_widget.py
@@ -1,11 +1,18 @@
 from PySide6.QtWidgets import (
     QWidget,
+    QPushButton,
+    QFileDialog,
 )
 from PySide6.QtGui import (
     QPalette,
 )
 from PySide6.QtCore import (
     Qt,
+    QUrl,
+)
+from PySide6.QtMultimedia import (
+    QMediaPlayer,
+    QAudioOutput
 )
 
 # TODO: add functionality
@@ -20,3 +27,34 @@ class AudioPlayerWidget(QWidget):
 
         self.setAutoFillBackground(True)
         self.setPalette(palette)
+
+        # Creating Buttons
+        self.button_play = QPushButton('Play', self)
+        self.button_play.resize(50, 50)
+        self.button_play.move(0, 210)
+        self.button_play.clicked.connect(self.play)
+        self.button_pause = QPushButton('Pause', self)
+        self.button_pause.resize(50, 50)
+        self.button_pause.move(50, 210)
+        self.button_pause.clicked.connect(self.pause)
+        self.button_track_selection = QPushButton('Select', self)
+        self.button_track_selection.resize(50, 50)
+        self.button_track_selection.move(100, 210)
+        self.button_track_selection.clicked.connect(self.seletcion_track)
+
+    def play(self):
+        self.audio_player = QMediaPlayer()
+        
+        self.audioOutput = QAudioOutput()
+        self.audio_player.setAudioOutput(self.audioOutput)
+        self.audio_player.setSource(QUrl.fromLocalFile(self.dialogue[0]))
+        self.audioOutput.setVolume(50)
+
+        self.audio_player.play()
+ 
+    def pause(self):
+        pass
+
+    def seletcion_track(self):
+        self.dialogue = QFileDialog.getOpenFileName(self,"Выбрать файл", '', '*mp3')
+        print(self.dialogue[0])
