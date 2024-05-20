28b8324fa60e0a81c90e6a11b42fea1c3ee13562
unknown <frogibogidogi@gmail.com>
2024-05-20 15:45:47 +1000

    audio player buttons

--- soni/ui/widgets/audio_player_widget.py
+++ soni/ui/widgets/audio_player_widget.py
@@ -2,9 +2,14 @@ from PySide6.QtWidgets import (
     QWidget,
     QPushButton,
     QFileDialog,
+    QHBoxLayout,
+    QVBoxLayout,
+
 )
 from PySide6.QtGui import (
     QPalette,
+    QPixmap,
+    QIcon,
 )
 from PySide6.QtCore import (
     Qt,
@@ -15,6 +20,8 @@ from PySide6.QtMultimedia import (
     QAudioOutput
 )
 
+import res.resources_rc
+
 # TODO: add functionality
 class AudioPlayerWidget(QWidget):
     def __init__(self, parent: QWidget | None = None) -> None:
@@ -23,24 +30,56 @@ class AudioPlayerWidget(QWidget):
         self.setMinimumSize(30, 30)
 
         palette = QPalette()
-        palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.magenta)
+        palette.setColor(QPalette.ColorRole.Window, Qt.GlobalColor.black)
 
         self.setAutoFillBackground(True)
         self.setPalette(palette)
 
+        self.pause_triger = False
+
+        # self.play_img1 = 
         # Creating Buttons
-        self.button_play = QPushButton('Play', self)
-        self.button_play.resize(50, 50)
-        self.button_play.move(0, 210)
+        self.hlayout_mid = QHBoxLayout()
+        self.hlayout_bottomn = QHBoxLayout()
+        self.vlayout = QVBoxLayout(self)
+
+        self.button_play = QPushButton()
+        self.button_play.setIcon(QPixmap(":/icons/play-black.svg"))
         self.button_play.clicked.connect(self.play)
-        self.button_pause = QPushButton('Pause', self)
-        self.button_pause.resize(50, 50)
-        self.button_pause.move(50, 210)
+
+        self.button_pause = QPushButton('Pause')
         self.button_pause.clicked.connect(self.pause)
-        self.button_track_selection = QPushButton('Select', self)
-        self.button_track_selection.resize(50, 50)
-        self.button_track_selection.move(100, 210)
+
+        self.button_track_selection = QPushButton('Select')
         self.button_track_selection.clicked.connect(self.seletcion_track)
+
+        self.button_random_order = QPushButton('Random')
+        self.button_random_order.clicked.connect(self.random_order)
+
+        self.button_loop = QPushButton('Loop')
+        self.button_loop.clicked.connect(self.loop)
+
+        self.button_next = QPushButton('Next')
+        self.button_next.clicked.connect(self.next)
+
+        self.button_past = QPushButton('Past')
+        self.button_past.clicked.connect(self.past)
+
+        self.hlayout_mid.addStretch(1)
+        self.hlayout_mid.addWidget(self.button_past)
+        self.hlayout_mid.addWidget(self.button_play)
+        self.hlayout_mid.addWidget(self.button_next)
+        self.hlayout_mid.addStretch(1)
+        self.hlayout_bottomn.addWidget(self.button_random_order)
+        self.hlayout_bottomn.addStretch(1)
+        self.hlayout_bottomn.addWidget(self.button_loop)
+        self.vlayout.addStretch(1)
+        self.vlayout.addLayout(self.hlayout_mid)
+        self.vlayout.addStretch(1)
+        self.vlayout.addLayout(self.hlayout_bottomn)
+        self.setLayout(self.vlayout)
+
+
 
     def play(self):
         self.audio_player = QMediaPlayer()
@@ -53,8 +92,25 @@ class AudioPlayerWidget(QWidget):
         self.audio_player.play()
  
     def pause(self):
-        pass
+        if self.pause_triger == False:
+            self.audio_player.pause()
+            self.pause_triger = True
+        else:
+            self.audio_player.play()
+            self.pause_triger = False
 
     def seletcion_track(self):
         self.dialogue = QFileDialog.getOpenFileName(self,"Выбрать файл", '', '*mp3')
         print(self.dialogue[0])
+
+    def loop(self):
+        pass
+
+    def next(self):
+        pass
+
+    def past(self):
+        pass
+
+    def random_order(self):
+        pass
